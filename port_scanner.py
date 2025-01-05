625e1d7a5c4800436ddac1709155018b47d4da4f
Mohammed Salih <mohammed.salih2@usmba.ac.ma>
2025-01-05 23:32:20 +0000

    Resolve target to ip or domain and fix verbose mode

--- port_scanner.py
+++ port_scanner.py
@@ -1,26 +1,55 @@
 import socket
+import ipaddress
+import re
 
 from common_ports import ports_and_services
 
-TIMEOUT = 5.0
+TIMEOUT = 2.0
+
+def resolve_target(target):
+    if re.match(r'^(?:\d{1,3}\.){3}\d{1,3}$', target) is not None:
+        try:
+            ip = str(ipaddress.ip_address(target))
+            try:
+                domains = socket.gethostbyaddr(ip)
+                return (ip, domains[0] if domains.count != 0 else None)
+            except Exception:
+                return (ip, None)
+        except Exception:
+            raise Exception("Error: Invalid IP address")
+    else:
+        try:
+            return socket.gethostbyname(target)
+        except Exception:
+            raise Exception("Error: Invalid hostname")
 
 def get_open_ports(target, port_range, verbose = False):
     if verbose:
         open_ports = ""
     else:
         open_ports = []
+    try:
+        ip = resolve_target(target)
+    except Exception as e:
+        return str(e)
+    if isinstance(ip, tuple):
+        target = ip[1]
+        ip     = ip[0]
+    if verbose:
+        print("rgegeger", target, ip)
+        open_ports += "Open ports for {}\n{:<7}  SERVICE"\
+            .format(
+                f"{target} ({ip})" if target is not None else f"{ip}",
+                "PORT"
+                )
 
     for port in range(port_range[0], port_range[1] + 1):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(TIMEOUT)
-        if verbose:
-            open_ports += "Open ports for {} ({})\nPORT\tSERVICE\n"\
-                .format(target, sock.getpeername()[0])
-
         try:
-            sock.connect((target, port))
+            sock.connect((ip, port))
             if verbose:
-                open_ports += "{}\t{}\n"\
+                open_ports += "\n{:<7}  {}"\
                     .format(
                         port,
                         ports_and_services.get(port, "Unknown")
